7acfb05861077e4d9a76b7264a3c4a4bb2aa1bb2
Vlad <x.vlad2101@gmail.com>
2023-12-01 13:29:14 +0200

    first push portfolio

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import styles from './style.module.css';
+
+function App() {
+    return (
+    <div className="App">
+      <header className="App-header">
+        <div className={styles.container}>
+          mama mia
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
