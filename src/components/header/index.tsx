a89c5b28d7372ab3fb808ba35ab05a888e2b3749
Vlad <x.vlad2101@gmail.com>
2023-12-03 16:15:59 +0200

    add animation in home and make animated button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,28 @@
 // modules
 import React from "react";
-import { Link } from "react-scroll";
+import {Link} from "react-scroll";
 // assets
-import { Logo } from "../../assets";
+import {Logo} from "../../assets";
 // constants
-import { headerLinks, HeaderLinksProps } from "../../constants/headerLinks";
+import {headerLinks, HeaderLinksProps} from "../../constants/headerLinks";
 // styles
 import styles from "./styles.module.css";
 
 const Header = () => {
-  return (
-    <div className={styles.container}>
-      <div>
-        <Logo />
-      </div>
-      <div className={styles.linkContainer}>
-        {headerLinks.map((item: HeaderLinksProps) => (
-          <Link to={item.path} className={styles.headerBtn}>
-            <span>{item.name}</span>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div>
+                <Logo/>
+            </div>
+            <div className={styles.linkContainer}>
+                {headerLinks.map((item: HeaderLinksProps) => (
+                    <Link to={item.path} className={styles.headerBtn} key={item.id}>
+                        <span>{item.name}</span>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Header;
