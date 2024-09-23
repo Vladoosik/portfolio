2707e48066231bed656ce64e28d4bef206439f5f
vladoosik <x.vlad2101@gmail.com>
2024-09-23 13:16:59 +0300

    start work on kloki kase

--- src/components/allWorkModal/index.tsx
+++ src/components/allWorkModal/index.tsx
@@ -1,78 +1,78 @@
 // modules
-import React, { FC, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import React, {FC, useEffect} from "react";
+import {AnimatePresence, motion} from "framer-motion";
 // components
-import { Button, CardModal } from "../index";
+import {Button, CardModal} from "../index";
 // styles
 import "./style.css";
 
 interface AllWorkModalProps {
-  data: any;
-  active: boolean;
-  setActive: (b: boolean) => void;
+    data: any;
+    active: boolean;
+    setActive: (b: boolean) => void;
 }
 
 const AllWorkModal: FC<AllWorkModalProps> = (props) => {
-  const { data, active, setActive } = props;
+    const {data, active, setActive} = props;
 
-  useEffect(() => {
-    const body = document.querySelector("body");
-    if (active) {
-      body?.classList.add("modal-open");
-    } else {
-      body?.classList.remove("modal-open");
-    }
-  }, [active]);
+    useEffect(() => {
+        const body = document.querySelector("body");
+        if (active) {
+            body?.classList.add("modal-open");
+        } else {
+            body?.classList.remove("modal-open");
+        }
+    }, [active]);
 
-  const handleModalClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-  };
+    const handleModalClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+    };
 
-  const modalVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
-    exit: { opacity: 0 },
-  };
+    const modalVariants = {
+        hidden: {opacity: 0},
+        visible: {opacity: 1},
+        exit: {opacity: 0},
+    };
 
-  const transition = { duration: 0.3, ease: "easeInOut" };
+    const transition = {duration: 0.3, ease: "easeInOut"};
 
-  return (
-    <AnimatePresence>
-      {active && (
-        <motion.div
-          className="modalWork active"
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          variants={modalVariants}
-          transition={transition}
-          onClick={() => setActive(false)}
-        >
-          <div className="buttonBox">
-            <Button
-              text="X Close"
-              onClick={() => setActive(false)}
-              widthArrow={false}
-              className={"buttonModal"}
-            />
-          </div>
-          <motion.div
-            className="cardModalContainer"
-            onClick={handleModalClick}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={modalVariants}
-            transition={transition}
-          >
-            {data.map((item: any, index: number) => (
-              <CardModal item={item} index={index} key={item.id} />
-            ))}
-          </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
+    return (
+        <AnimatePresence>
+            {active && (
+                <motion.div
+                    className="modalWork active"
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    variants={modalVariants}
+                    transition={transition}
+                    onClick={() => setActive(false)}
+                >
+                    <div className="buttonBox">
+                        <Button
+                            text="X Close"
+                            onClick={() => setActive(false)}
+                            widthArrow={false}
+                            className={"buttonModal"}
+                        />
+                    </div>
+                    <motion.div
+                        className="cardModalContainer"
+                        onClick={handleModalClick}
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        variants={modalVariants}
+                        transition={transition}
+                    >
+                        {data?.map((item: any, index: number) => (
+                            <CardModal item={item} index={index} key={item.id}/>
+                        ))}
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
 };
 
 export default AllWorkModal;
--- src/screens/klokiCase/index.tsx
+++ src/screens/klokiCase/index.tsx
@@ -1,106 +1,117 @@
 // modules
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { NavigateFunction } from "react-router/dist/lib/hooks";
-import { animateScroll } from "react-scroll";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {NavigateFunction} from "react-router/dist/lib/hooks";
+import {animateScroll} from "react-scroll";
 // components
 import {
-  AboutProject,
-  AllWorkModal,
-  Footer,
-  Header,
-  Introduction,
-  Modal,
-  WorkDescription,
-  WorkNavigator,
+    AboutProject,
+    AllWorkModal,
+    AnimatedText,
+    Footer,
+    Header,
+    Introduction,
+    Modal,
+    WorkDescription,
+    WorkNavigator,
 } from "../../components";
 // utils
 import transitionPages from "../../utils/transitionPages";
-import { handleNavigation } from "../../utils/navigation";
+import {handleNavigation} from "../../utils/navigation";
 // constants
-import { caseHeaderLinks } from "../../constants/headerLink/headerLinks";
-import { PlanerColor } from "../../constants/projectColors";
+import {caseHeaderLinks} from "../../constants/headerLink/headerLinks";
+import {PlanerColor} from "../../constants/projectColors";
 // styles
 import styles from "./styles.module.css";
-import { modalWorkArr } from "../../constants/commerceProject";
+import {modalWorkArr} from "../../constants/commerceProject";
 
 const PlannerCase = () => {
-  const [active, setActive] = useState<boolean>(false);
-  const [workModal, setWorkModal] = useState<boolean>(false);
-  const navigation: NavigateFunction = useNavigate();
+    const [active, setActive] = useState<boolean>(false);
+    const [workModal, setWorkModal] = useState<boolean>(false);
+    const navigation: NavigateFunction = useNavigate();
+    const image = require("../../assets/png/kloki_main_menu.png");
 
-  const scrollToTop = () => {
-    animateScroll.scrollToTop();
-  };
+    const scrollToTop = () => {
+        animateScroll.scrollToTop();
+    };
 
-  return (
-    <>
-      <div className={styles.container}>
-        <Header
-          setWorkModal={setWorkModal}
-          alternative
-          data={caseHeaderLinks}
-          setModal={setActive}
-          navigation={navigation}
-        />
-        <div className={styles.contentBox}>
-          <p>For Dream Case</p>
-          <WorkDescription
-            role={"React-Native Developer"}
-            context={"Mobile Planer"}
-            period={"January 2024 - Present"}
-            titleColor={"#7AAC2D"}
-          />
-        </div>
-        <div>
-          <Introduction
-            title={"About Project"}
-            description={
-              "The project created in React-Native is a planner" +
-              " that allows you to add, delete plans, daily habits," +
-              " monitor your progress, and also receive notifications " +
-              "from the application about upcoming plans"
-            }
-          />
-        </div>
-        <AboutProject
-          data={PlanerColor}
-          description={
-            <p className={styles.workDescription}>
-              My job was to <strong>develop the application</strong>, create new
-              functionality, support and optimize old solutions in the
-              application, as well as <strong>connect the server</strong>{" "}
-              component of the application.
-              <br />
-              <br />
-              Overall, the project is quite large and I learned a lot during its
-              development. In this case <strong>
-                MobX and Typescript
-              </strong>{" "}
-              were actively used, which helped me understand them in{" "}
-              <strong>more detail</strong>. Of course, there were other
-              libraries that can often be found in react-native, for example{" "}
-              <strong>React-Navigation</strong>
-            </p>
-          }
-          photoSource={require("../../assets/png/planer_mobile.png")}
-        />
-        <WorkNavigator
-          nextProjectName={"ForDream Web"}
-          onClick={() => handleNavigation(navigation, "/forDream")}
-          overlayColor={"#030D22"}
-          backgroundColor={"white"}
-        />
-        <Footer onLinkPress={scrollToTop} />
-        <Modal active={active} setActive={setActive} />
-        <AllWorkModal
-          data={modalWorkArr}
-          active={workModal}
-          setActive={setWorkModal}
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.container}>
+                <Header
+                    setWorkModal={setWorkModal}
+                    alternative
+                    data={caseHeaderLinks}
+                    setModal={setActive}
+                    navigation={navigation}
+                />
+                <div className={styles.contentBox}>
+                    <div className={styles.titleContainer}>
+                        <div className={styles.titleContent}>
+                            <p className={styles.title}>Kloki</p>
+                            <AnimatedText
+                                text={"All your plans in one app"}
+                                className={styles.description}
+                            />
+                        </div>
+                        <img className={styles.macImg} src={image} alt="macImage"/>
+                    </div>
+                    <WorkDescription
+                        role={"React-Native Developer"}
+                        context={"Mobile Planer"}
+                        period={"January 2024 - Present"}
+                        titleColor={"#7AAC2D"}
+                    />
+                </div>
+                <div>
+                    <Introduction
+                        title={"About Project"}
+                        description={
+                            "The project created in React-Native is a planner" +
+                            " that allows you to add, delete plans, daily habits," +
+                            " monitor your progress, and also receive notifications " +
+                            "from the application about upcoming plans"
+                        }
+                    />
+                </div>
+                <AboutProject
+                    data={PlanerColor}
+                    description={
+                        <p className={styles.workDescription}>
+                            My job was to <strong>develop the application</strong>, create new
+                            functionality, support and optimize old solutions in the
+                            application, as well as <strong>connect the server</strong>{" "}
+                            component of the application.
+                            <br/>
+                            <br/>
+                            Overall, the project is quite large and I learned a lot during its
+                            development. In this case <strong>
+                            MobX and Typescript
+                        </strong>{" "}
+                            were actively used, which helped me understand them in{" "}
+                            <strong>more detail</strong>. Of course, there were other
+                            libraries that can often be found in react-native, for example{" "}
+                            <strong>React-Navigation</strong>
+                        </p>
+                    }
+                    photoSource={require("../../assets/png/planer_mobile.png")}
+                />
+                <WorkNavigator
+                    nextProjectName={"ForDream Web"}
+                    onClick={() => handleNavigation(navigation, "/forDream")}
+                    overlayColor={"#030D22"}
+                    backgroundColor={"white"}
+                />
+                <Footer onLinkPress={scrollToTop}/>
+                <Modal active={active} setActive={setActive}/>
+                <AllWorkModal
+                    data={modalWorkArr}
+                    active={workModal}
+                    setActive={setWorkModal}
+                />
+            </div>
+        </>
+    );
 };
 
 export default transitionPages(PlannerCase);
